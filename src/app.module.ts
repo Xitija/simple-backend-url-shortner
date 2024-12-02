734b4ead4cfe49904be7372c4d4ab2c8b863ac10
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-12-02 20:10:35 +0530

    chore : packages no longer used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ScheduleModule } from '@nestjs/schedule';
+// import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot()],
+  imports: [
+    // ScheduleModule.forRoot(),
+    ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
