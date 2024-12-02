62222972a6eab64ea6d1bc14da9d7866314c36e6
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-12-02 21:21:10 +0530

    chore : added api description

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,11 @@ async function bootstrap() {
     new FastifyAdapter()
   );
   const config = new DocumentBuilder()
-    .setTitle('Simple Nest Fastify example')
+    .setTitle('Url Shortner')
+    .setDescription(`The working of a URL shortener is straightforward. In the short URL generation part, the API takes in a POST request with the original URL as a payload. A unique ID is generated that corresponds to the original URL. This ID is added to the end of the base URL, i.e., the URL of your application.
+    The generated URL and the original URL are stored in the database.
+
+    Tech Stack: Postgres, Prisma, NestJS-Fastify`)
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
